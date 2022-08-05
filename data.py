4bdbc15ff79003f90828462a8d5fd76657992a19
Stanko Kovacevic <stkovacevic94@gmail.com>
2022-08-05 12:41:22 +0200

    Revert data handling to old way

--- data.py
+++ data.py
@@ -1,68 +1,60 @@
-import dataclasses
 from os import path
 
+import pandas as pd
+import torch
 import tqdm
-import numpy as np
 import minerl
 from torch.utils.data import Dataset
-from torch.utils.data.dataset import T_co
 
 
-@dataclasses.dataclass(frozen=True)
-class Transitions(Dataset):
-    obs: np.ndarray
-    acts: np.ndarray
+class MineRLDataset(Dataset):
+    def __init__(self, wrapped_env, save_path, remove_noop=True) -> None:
+        env_id = wrapped_env.unwrapped.spec.id
+        self.wrapped_env = wrapped_env
 
-    def __post_init__(self):
-        """Performs input validation: check shapes & dtypes match docstring.
-        Also make array values read-only.
-        Raises:
-            ValueError: if batch size (array length) is inconsistent
-                between `obs`, `acts` and `infos`.
-        """
-        for val in vars(self).values():
-            if isinstance(val, np.ndarray):
-                val.setflags(write=False)
-
-        if len(self.obs) != len(self.acts):
-            raise ValueError(
-                "obs and acts must have same number of timesteps: "
-                f"{len(self.obs)} != {len(self.acts)}",
+        # Download data if needed
+        if not path.exists(path.join(save_path, env_id)):
+            minerl.data.download(
+                directory=save_path,
+                environment=env_id,
+                update_environment_variables=False,
+                disable_cache=True
             )
 
+        data_handle = minerl.data.make(env_id, save_path)
+        data = []
+        for trajectory_idx, stream_name in enumerate(tqdm.tqdm(data_handle.get_trajectory_names()[:2])):
+            for time_step, transition in enumerate(
+                    data_handle.load_data(stream_name=stream_name)):
+                obs, action, reward, next_obs, done = transition
+
+                if remove_noop and wrapped_env.wrap_action(action) != 7:
+                    data.append({
+                        "trajectory_idx": trajectory_idx,
+                        "time_step": time_step,
+                        "obs": obs,
+                        "action": action,
+                        "reward": reward,
+                        "next_obs": next_obs,
+                        "done": done
+                    })
+
+        self.transitions = pd.DataFrame(data)
+
     def __len__(self):
-        """Returns number of transitions. Always positive."""
-        return len(self.obs)
+        return self.transitions.shape[0]
 
-    def __getitem__(self, index) -> T_co:
-        return self.obs[index], self.acts[index]
+    def __getitem__(self, idx):
+        item = self.transitions.iloc[idx]
 
+        wrapped_obs = self.wrapped_env.observation(item['obs'])
+        wrapped_next_obs = self.wrapped_env.observation(item['next_obs'])
+        wrapped_action = self.wrapped_env.wrap_action(item['action'])
 
-def get_dataset(wrapped_env, save_path, fast_dev_run: bool = False) -> Transitions:
-    env_id = wrapped_env.unwrapped.spec.id
+        th_wrapped_obs = torch.tensor(wrapped_obs, dtype=torch.float32)
+        th_action = torch.tensor(wrapped_action, dtype=torch.long)
+        th_reward = torch.tensor(item['reward'], dtype=torch.float32)
+        th_wrapped_next_obs = torch.tensor(wrapped_next_obs, dtype=torch.float32)
+        th_done = torch.tensor(item['done'])
 
-    # Download data if needed
-    if not path.exists(path.join(save_path, env_id)):
-        minerl.data.download(
-            directory=save_path,
-            environment=env_id,
-            update_environment_variables=False,
-            disable_cache=True
-        )
-
-    data_handle = minerl.data.make(env_id, save_path)
-    observations = []
-    actions = []
-    trajectories_to_load = data_handle.get_trajectory_names()
-    if fast_dev_run:
-        trajectories_to_load = data_handle.get_trajectory_names()[:2]
-    for trajectory_idx, stream_name in enumerate(tqdm.tqdm(trajectories_to_load)):
-        for time_step, transition in enumerate(
-                data_handle.load_data(stream_name=stream_name)):
-            obs, action, reward, next_obs, done = transition
-            wrapped_obs = wrapped_env.observation(obs) if hasattr(wrapped_env, 'observation') else obs
-            wrapped_action = wrapped_env.wrap_action(action) if hasattr(wrapped_env, 'wrap_action') else action
-            if wrapped_action != 7:
-                observations.append(wrapped_obs.squeeze())
-                actions.append(wrapped_action)
-    return Transitions(np.stack(observations), np.array(actions, dtype=np.longlong))
+        return th_wrapped_obs, th_action, th_reward, th_wrapped_next_obs, th_done
